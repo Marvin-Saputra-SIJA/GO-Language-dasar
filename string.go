8eaa8355470ad8bdc9d4c3b550313bb5af6e6925
Marvin-Saputra-SIJA <marvinputra414@gmail.com>
2022-07-23 21:28:11 +0700

    adding go script but not compiled

---
+++ string.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main () {
+	fmt.Println(len("Marvin Saputra")) // len function is used to calculating amount of string
+	fmt.Print("Marvin Saputra"[2]) // [2] used for take a letter from a string representation of index 
+}
---
+++ variable.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main () {
+
+	var (
+		name = "Marvin Saputra";
+		kelas = "XI SIJA 2"
+		mapel = "Software Computer"
+		nilai = 90
+		ket = true	
+		
+	)
+
+	fmt.Println(name,kelas,mapel,nilai,ket)
+}
